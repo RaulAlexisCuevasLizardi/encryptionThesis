2258f04143a09fab2e4cf074997737285d0d9d79
RaulAlexisCuevasLizardi <33424153+RaulAlexisCuevasLizardi@users.noreply.github.com>
2017-11-06 12:25:22 -0400

    Update Picture.java

--- Picture.java
+++ Picture.java
@@ -1,1 +1,42 @@
+package test;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Picture {
+	private static File f;
+	private static BufferedImage img;
+	private final int width;
+	private final int height;
+	private ARGBPixel pixels[][];
+	
+	public Picture(String filepath) {
+		
+	}
+	
+	public Picture(ARGBPixel[][] pixels){
+		this.pixels = pixels;
+		this.width = this.pixels.length;
+		this.height = this.pixels[0].length;
+	}
+
+	public ARGBPixel[][] getPixels() {
+		return pixels;
+	}
+
+	public void setPixels(ARGBPixel[][] pixels) {
+		this.pixels = pixels;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+}
 
