33aec4cdc050c6077230bf5236d8dbdf9e7b8203
RaulAlexisCuevasLizardi <33424153+RaulAlexisCuevasLizardi@users.noreply.github.com>
2017-11-08 10:05:59 -0400

    Update Picture.java
    Added a randomizePixels method that uses the changeSpecificPixel method to create a new image pixel by pixel.
    
    Fixed the toString method so that it correctly returns all fields values.

--- Picture.java
+++ Picture.java
@@ -3,6 +3,8 @@ package test;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -23,6 +25,16 @@ public class Picture {
 		height = img.getHeight();
 		width = img.getWidth();
 		pixels = new ARGBPixel[width][height];
+		
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				try {
+					pixels[i][j] = new ARGBPixel(img.getRGB(i, j));
+				} catch (PixelValueException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 	public Picture(ARGBPixel[][] pixels){
@@ -51,12 +63,37 @@ public class Picture {
 		return img;
 	}
 	
+	@Override
+	public String toString() {
+		String temp = "";
+		for(ARGBPixel[] pix: pixels)
+			temp += Arrays.toString(pix) + ", ";
+		return "Picture [width=" + width + ", height=" + height + ", pixels="
+				+ temp + ", image= " + img.toString() + ", file= " + f.toString() + "]";
+	}
+
 	public void export(String outputFilename){
 		try{
 			f = new File("C:\\Users\\841111795\\Desktop\\" + outputFilename + ".jpg");
 			ImageIO.write(img, "jpg", f);
 		}catch(IOException e){
 			System.out.println(e);
+		}
+	}
+	
+	public void randomizePixels(){
+		for(int i = 0; i < width; i++)
+			for(int j = 0; j < height; j++)
+				changeSpecificPixel(i, j);
+	}
+	
+	private void changeSpecificPixel(int i, int j){
+		int color = new Random().nextInt();
+		img.setRGB(i, j, color);
+		try {
+			pixels[i][j].setP(color);
+		} catch (PixelValueException e) {
+			e.printStackTrace();
 		}
 	}
 }
