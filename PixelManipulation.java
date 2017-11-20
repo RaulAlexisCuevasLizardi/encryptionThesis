8dde82bb535c7e960fb69fb07eb6de6f997e9ec2
RaulAlexisCuevasLizardi <33424153+RaulAlexisCuevasLizardi@users.noreply.github.com>
2017-11-20 10:52:46 -0400

    Update PixelManipulation.java

--- PixelManipulation.java
+++ PixelManipulation.java
@@ -1,12 +1,8 @@
-package test;
+package version_1;
 import java.io.File;
 import java.util.Scanner;
 import java.awt.image.BufferedImage;
 
-/*
- * TODO
- * TIME TO START WORKING ON THE ENCRYPTION ALGORITHM
- */
 
 public class PixelManipulation {
 	static BufferedImage img = null;
@@ -16,53 +12,36 @@ public class PixelManipulation {
 	static Scanner kb;
 	public static void main(String[] args) throws PixelValueException {
 		kb = new Scanner(System.in);
-		pic = new Picture("C:\\Users\\841111795\\Desktop\\HappyFace.png");
+		pic = new Picture("C:\\Users\\841111795\\Desktop\\images.png");
+		PictureEncryptor c = new PictureEncryptor(pic);
 		
-//		System.out.println(img.toString());
-//		System.out.println(img.getMinTileX());
-//		System.out.println(img.getMinTileY());
-//		System.out.println(img.getMinX());
-//		System.out.println(img.getMinY());
-//		System.out.println(img.getRGB(1, 0));
-		
-//		pixels = new ARGBPixel[img.getWidth()][img.getHeight()];
-//		for(int i = 0; i < img.getWidth(); i++){
-//			for(int j = 0; j < img.getHeight(); j++){
-//				pixels[i][j] = new ARGBPixel(img.getRGB(i, j));
-//			}
-//		}
-//
-//
-//		for(int i = 0; i < img.getWidth(); i++){
-//			for(int j = 0; j < img.getHeight(); j++){
-//				int color = new Random().nextInt();
-//				System.out.println("color " + color);
-//				img.setRGB(i, j, color);
-//			}
-//		}
-		System.out.println(pic);
-		System.out.println("Enter the file name of the picture to be exported.");
+		System.out.println("Enter the file name of the picture that will be encrypted.");
 		String outputFilename = kb.nextLine();
-		kb.nextLine();
-		pic.export(outputFilename); 
-		System.out.println("Exported. Press enter...");
-		kb.nextLine();
+		c.Encrypt();
+		c.ExportPicture(outputFilename);
 		
-		System.out.println("Sorting...");
-		pic.pixelSort();
-		kb.nextLine();
-		System.out.println("Enter a different file name for the sorted picture to be exported.");
-		outputFilename = kb.nextLine();
-		pic.export(outputFilename);
-		System.out.println("Exported. Press enter...");
-		kb.nextLine();
-		
-		System.out.println("Randomizing...");
-		pic.randomizePixels();
-		System.out.println("Enter a different file name for the randomized picture to be exported.");
-		outputFilename = kb.nextLine();
-		pic.export(outputFilename);
-		System.out.println("Exported.");
+//		System.out.println(pic);
+//		System.out.println("Enter the file name of the copy of the picture to be exported.");
+//		String outputFilename = kb.nextLine();
+//		pic.export(outputFilename); 
+//		System.out.println("Exported. Press enter...");
+//		kb.nextLine();
+//		
+//		System.out.println("Sorting...");
+//		pic.pixelSort();
+//		kb.nextLine();
+//		System.out.println("Enter a different file name for the sorted picture to be exported.");
+//		outputFilename = kb.nextLine();
+//		pic.export(outputFilename);
+//		System.out.println("Exported. Press enter...");
+//		kb.nextLine();
+//		
+//		System.out.println("Randomizing...");
+//		pic.randomizePixels();
+//		System.out.println("Enter a different file name for the randomized picture to be exported.");
+//		outputFilename = kb.nextLine();
+//		pic.export(outputFilename);
+//		System.out.println("Exported.");
 		
 	}
 }
